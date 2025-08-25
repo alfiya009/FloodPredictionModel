f9abe3e5a012c1c2b227a5615159e0c1e1649d5c
Alfiya qureshi <147529869+Alfiyaqureshi@users.noreply.github.com>
2025-08-25 16:49:36 +0530

    add requirements

--- app.py
+++ app.py
@@ -157,4 +157,6 @@ def index():
                            **(weather_data or {}))
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
+
